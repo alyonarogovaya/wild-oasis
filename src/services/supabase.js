b2593d63ddb5dbf8fe6e8006c34dc127c7256356
Alyona Rogovaya <alyona@yelk.com.ua>
2025-06-30 18:25:18 +0200

    Added a supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://qzkukktaabbdsorfvsst.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6a3Vra3RhYWJiZHNvcmZ2c3N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyOTc5NzMsImV4cCI6MjA2Njg3Mzk3M30.cWl1PyaP3XXH7KAf6DiXIxx4_yWd6hsUsh9GuNrociw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
