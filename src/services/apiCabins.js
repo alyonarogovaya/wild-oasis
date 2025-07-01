bc7867400ed0e8d116dfa629c3c89202d60cb7f3
Alyona Rogovaya <alyona@yelk.com.ua>
2025-07-01 21:39:49 +0200

    Added a cabin creating

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,5 @@
 import supabase from "./supabase";
+import { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -17,6 +18,30 @@ export async function deleteCabin(id) {
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be deleted");
+  }
+
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll('/', '')
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`
+
+  const { data, error } = await supabase.from("cabins").insert([{...newCabin, image: imagePath}]);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
+  }
+
+  const { error: storageError } = await supabase
+  .storage
+  .from('cabin-images')
+  .upload(imageName, newCabin.image)
+
+  if (storageError) {
+      await supabase.from("cabins").delete().eq("id", data.id);
+      console.error(storageError);
+      throw new Error("Cabin image could not be uploaded and cabin was not created");
   }
 
   return data;
