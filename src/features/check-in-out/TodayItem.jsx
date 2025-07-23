6ffb0ffbf711d241188b3c864c1885cea30eb108
Alyona Rogovaya <alyona@yelk.com.ua>
2025-07-23 16:52:30 +0200

    Added a today activities list

--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Tag from "../../ui/Tag";
+import Button from "../../ui/Button";
+import CheckoutButton from "./CheckoutButton";
+import { Flag } from "../../ui/Flag";
 
 const StyledTodayItem = styled.li`
   display: grid;
@@ -18,3 +23,35 @@ const StyledTodayItem = styled.li`
 const Guest = styled.div`
   font-weight: 500;
 `;
+
+function TodayItem({ activity }) {
+  const { id, status, guests, numNights } = activity;
+  const isUnconfirmed = status === "unconfirmed";
+  const isCheckedIn = status === "checked-in";
+  console.log(activity);
+
+  return (
+    <StyledTodayItem>
+      {isUnconfirmed && <Tag type="green">Arriving</Tag>}
+      {isCheckedIn && <Tag type="blue">Departing</Tag>}
+
+      <Flag src={guests.countryFlag} alt={`Flag of ${guests.countryFlag}`} />
+      <Guest>{guests.fullName}</Guest>
+      <div>{numNights} nights</div>
+
+      {isUnconfirmed && (
+        <Button
+          size="small"
+          variation="primary"
+          as={Link}
+          to={`/checkin/${id}`}
+        >
+          Check in
+        </Button>
+      )}
+      {isCheckedIn && <CheckoutButton bookingId={id} />}
+    </StyledTodayItem>
+  );
+}
+
+export default TodayItem;
